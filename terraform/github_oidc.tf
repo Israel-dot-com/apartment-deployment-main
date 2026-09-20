495fb1359d32464f4902edeb26c33d7d43698fd6
Israel <isltimi1000@gmail.com>
2026-09-20 06:19:57 +0100

    fixed lowercase tf repo

--- terraform/github_oidc.tf
+++ terraform/github_oidc.tf
@@ -32,7 +32,7 @@ resource "aws_iam_role" "github_actions" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:israel-dot-com/apartment-deployment-main:*"
+            "token.actions.githubusercontent.com:sub" = "repo:Israel-dot-com/apartment-deployment-main:*"
           }
         }
       }
